c4b0e4acae1094bc207888b7fa0a009b5b9f0cd1
dodger215 <kelvinenosdzah2@gmail.com>
2025-09-26 09:53:50 +0000

    Started

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // Create admin user
-        User::create([
+        User::firstOrCreate([
             'name' => 'Admin User',
             'email' => 'admin@campusstreet.com',
             'password' => Hash::make('admin123'),
